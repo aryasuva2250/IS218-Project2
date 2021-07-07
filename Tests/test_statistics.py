6846c03c504fcbf1c2bf74b96aa2e0c964dbc474
aryasuva2250 <as3526@njit.edu>
2021-07-07 10:26:10 -0400

    Feature: updated statistics test file

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -14,31 +14,63 @@ import pprint
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
+        '''
         self.testing = [1, 2, 3, 4, 5]
         self.testing1 = [2, 2, 4, 6, 9, 1]
         self.testing3 = [1, 10, 5, 4, 5]
         self.testing4 = [1, 8, 6,]
+        '''
         #seed(5)
         #self.testData = randint(0, 10, 20)
-        #self.statistics = Statistics()
+        self.statistics = Statistics()
 
     #def test_instantiate_calculator(self):
      #   self.assertIsInstance(self.statistics, Statistics)
 
     def test_mean_calculator(self):
-        self.assertEqual(3, mean.mean(self.testing))
+        statistics = Statistics()
+        random_generator = RandomGenerator()
+        testing = random_generator.random_list(self, 0, 100, 5, 4)
+        mean = statistics.mean(testing)
+        self.assertEqual(statistics.mean(testing), mean)
+        self.assertEqual(statistics.result, mean)
+        testing.clear()
 
     def test_median_calculator(self):
-        self.assertEqual(3, median.median(self.testing1))
+        statistics = Statistics()
+        random_generator = RandomGenerator()
+        testing = random_generator.random_list(self, 0, 100, 5, 4)
+        median = statistics.median(testing)
+        self.assertEqual(statistics.median(testing), median)
+        self.assertEqual(statistics.result, median)
+        testing.clear()
 
     def test_mode_calculator(self):
-        self.assertEqual(3, mode.mode(self.testing2))
+        statistics = Statistics()
+        random_generator = RandomGenerator()
+        testing = random_generator.random_list(self, 0, 100, 5, 4)
+        mode = statistics.mode(testing)
+        self.assertEqual(statistics.mode(testing), mode)
+        self.assertEqual(statistics.result, mode)
+        testing.clear()
 
     def test_standard_deviation_calculator(self):
-        self.assertEqual(3, standard_deviation.standard_deviation(self.testing3))
+        statistics = Statistics()
+        random_generator = RandomGenerator()
+        testing = random_generator.random_list(self, 0, 100, 5, 4)
+        standard_deviation = statistics.standard_deviation(testing)
+        self.assertEqual(statistics.standard_deviation(testing), standard_deviation)
+        self.assertEqual(statistics.result, standard_deviation)
+        testing.clear()
 
     def test_variance_calculator(self):
-        self.assertEqual(2, variance.variance(self.testing4))
+        statistics = Statistics()
+        random_generator = RandomGenerator()
+        testing = random_generator.random_list(self, 0, 100, 5, 4)
+        variance = statistics.variance(testing)
+        self.assertEqual(statistics.variance(testing), variance)
+        self.assertEqual(statistics.result, variance)
+        testing.clear()
 
 
 if __name__ == '__main__':
