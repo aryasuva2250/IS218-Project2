c6ad63de671a022a3b2b36e6cd0b2c694289807c
aryasuva2250 <as3526@njit.edu>
2021-07-07 10:38:44 -0400

    Feature: updated random generator test file for module not callable

--- Tests/test_random_generator.py
+++ Tests/test_random_generator.py
@@ -1,25 +1,25 @@
 import unittest
 
-from RandomGenerator import random_generator
+from RandomGenerator.random_generator import RandomGenerator
 
 class RandomGeneratorTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.random_generator = random_generator()
+        self.random_generator = RandomGenerator()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.random_generator, random_generator)
+        self.assertIsInstance(self.random_generator, RandomGenerator)
 
     def test_random_num_without_seed(self):
-        random_generator = self.random_generator()
+        random_generator = RandomGenerator()
         print(random_generator.random_num(self, 0, 100))
 
     def test_random_num_with_seed(self):
-        random_generator = self.random_generator()
+        random_generator = RandomGenerator()
         r = random_generator.random_seed(self, 0, 100, 10)
         self.assertEqual(random_generator.random_seed(self, 0, 100, 10), r)
 
     def test_random_list(self):
-        random_generator = self.random_generator()
+        random_generator = RandomGenerator()
         r = random_generator.random_list(self, 0, 100, 10, 10)
         self.assertEqual(random_generator.random_list(self, 0, 100, 10, 5), r)
 
