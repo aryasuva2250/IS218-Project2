1d21b1d097edc44b59781416c7f3a82a663577ec
aryasuva2250 <as3526@njit.edu>
2021-07-08 11:10:51 -0400

    Feature: updated addition and test csv tests

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -9,6 +9,38 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.Calculator = Calculator()
 
+    def test_instantiate_calculator(self):
+        calc = Calculator()
+        self.assertIsInstance(calc, Calculator)
+
+    def test_addition(self):
+        self.assertEqual(self.Calculator.add(2, 2), 4)
+        self.assertEqual(self.Calculator.result, 4)
+
+    def test_subtraction(self):
+        self.assertEqual(self.Calculator.subtract(2, 2), 0)
+        self.assertEqual(self.Calculator.result, 0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.Calculator.multiply(2, 2), 4)
+        self.assertEqual(self.Calculator.result, 4)
+
+    def test_division(self):
+        self.assertEqual(self.Calculator.divide(2, 2), 1)
+        self.assertEqual(self.Calculator.result, 1)
+        try:
+            self.Calculator.divide(1, 1)
+        except IndexError:
+            print('DivideByZeroError')
+
+    def test_square(self):
+        self.assertEqual(self.Calculator.square(2), 4)
+        self.assertEqual(self.Calculator.result, 4)
+
+    def test_square_root(self):
+        self.assertEqual(self.Calculator.sqrt(4), 2)
+        self.assertEqual(self.Calculator.result, 2)
+    '''
     def test_addition_reader(self):
         self.csv_reader = CsvReader('Tests/Data/Addition.csv')
         data = self.csv_reader.return_data_as_objects()
@@ -44,6 +76,7 @@ class MyTestCase(unittest.TestCase):
         data = self.csv_reader.return_data_as_objects()
         for row in data:
             self.assertEqual(self.Calculator.sqrt(row['Value 1']), row['Result'])
+    '''
     '''
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_objects('person')
