88daba7ffc55ed6dbc96fcccd15ffec82d13a7a5
aryasuva2250 <as3526@njit.edu>
2021-07-07 21:54:59 -0400

    Feature: updated median

--- Statistics/Median.py
+++ Statistics/Median.py
@@ -5,11 +5,12 @@ def median(x):
     x.sort()
     l = len(x)
     if((l % 2) == 0):
-        result = division(2, (addition(((x[division(2, l)]), subtraction(2, x[division(2, 1)])))))
+        result = ((x[l // 2]) + (x[l // 2]) - 1) // 2
+        #result = division(2, (addition(((x[division(2, l)]), subtraction(2, x[division(2, 1)])))))
         return result
         #m1 = x[division(2, l)]
         #m2 = x[subtraction(1, division(l, 2))]
         #result = division(2, (addition(m1, m2)))
     else:
-        result = x[division(2, l)]
+        result = x[(l // 2)]
         return result
