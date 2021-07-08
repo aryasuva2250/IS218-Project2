0394076c914b0204369985606f93bf900c5d053c
aryasuva2250 <as3526@njit.edu>
2021-07-08 10:59:24 -0400

    Feature: updated csv tests

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -13,37 +13,37 @@ class MyTestCase(unittest.TestCase):
         self.csv_reader = CsvReader('Tests/Data/Addition.csv')
         data = self.csv_reader.return_data_as_objects()
         for row in data:
-            self.assertEqual(self.Calculator.add(row['a'], row['b']), row['result'])
+            self.assertEqual(self.Calculator.add(row['Value 1'], row['Value 2']), row['Result'])
 
     def test_subtraction_reader(self):
         self.csv_reader = CsvReader('Tests/Data/Subtraction.csv')
         data = self.csv_reader.return_data_as_objects()
         for row in data:
-            self.assertEqual(self.Calculator.subtract(row['a'], row['b']), row['result'])
+            self.assertEqual(self.Calculator.subtract(row['Value 1'], row['Value 2']), row['Result'])
 
     def test_division_reader(self):
         self.csv_reader = CsvReader('Tests/Data/Division.csv')
         data = self.csv_reader.return_data_as_objects()
         for row in data:
-            self.assertEqual(self.Calculator.divide(row['a'], row['b']), row['result'])
+            self.assertEqual(self.Calculator.divide(row['Value 1'], row['Value 2']), row['Result'])
 
     def test_multiply_reader(self):
         self.csv_reader = CsvReader('Tests/Data/Multiplication.csv')
         data = self.csv_reader.return_data_as_objects()
         for row in data:
-            self.assertEqual(self.Calculator.multiply(row['a'], row['b']), row['result'])
+            self.assertEqual(self.Calculator.multiply(row['Value 1'], row['Value 2']), row['Result'])
 
     def test_square_reader(self):
         self.csv_reader = CsvReader('Tests/Data/Square.csv')
         data = self.csv_reader.return_data_as_objects()
         for row in data:
-            self.assertEqual(self.Calculator.squaring(row['a'], row['b']), row['result'])
+            self.assertEqual(self.Calculator.squaring(row['Value 1'], row['Value 2']), row['Result'])
 
     def test_square_root_reader(self):
         self.csv_reader = CsvReader('Tests/Data/SquareRoot.csv')
         data = self.csv_reader.return_data_as_objects()
         for row in data:
-            self.assertEqual(self.Calculator.sqrt(row['a'], row['b']), row['result'])
+            self.assertEqual(self.Calculator.sqrt(row['Value 1'], row['Value 2']), row['Result'])
     '''
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_objects('person')
