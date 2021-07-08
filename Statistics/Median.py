d583ed0dbcf2db77f0ea5d435e4b6fc06d3841d6
aryasuva2250 <as3526@njit.edu>
2021-07-07 21:45:51 -0400

    Feature: updated median and test population sampling

--- Statistics/Median.py
+++ Statistics/Median.py
@@ -4,10 +4,12 @@ from Calculator.subtraction import subtraction
 def median(x):
     x.sort()
     l = len(x)
-    if(l % 2 == 0):
-        m1 = x[division(2, l)]
-        m2 = x[subtraction(1, division(l, 2))]
-        result = division(2, (addition(m1, m2)))
+    if((l % 2) == 0):
+        result = division(2, (addition(((x[division(2, l)]), subtraction(2, x[division(2, 1)])))))
+        return result
+        #m1 = x[division(2, l)]
+        #m2 = x[subtraction(1, division(l, 2))]
+        #result = division(2, (addition(m1, m2)))
     else:
         result = x[division(2, l)]
-    return result
+        return result
