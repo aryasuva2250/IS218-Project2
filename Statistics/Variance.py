ff30aa0144a1c3bf7f6a37e90583b2b89caa094a
aryasuva2250 <as3526@njit.edu>
2021-07-07 22:56:44 -0400

    Feature: fixing variance

--- Statistics/Variance.py
+++ Statistics/Variance.py
@@ -1,9 +1,15 @@
+'''
 from Calculator.division import Division
 from Calculator.subtraction import Subtraction
 from Calculator.square import Square
 from Statistics.Mean import mean
+'''
+import statistics
 def variance(x):
+    '''
     x_mean = mean(x)
     result = sum([Square.square(x - x_mean)  for c in x]) / len(x)
     #result = Division.division(len(x), sum(Square.square((Subtraction.subtraction(x_mean, x)))))
     return result
+    '''
+    return statistics.variance(x)
